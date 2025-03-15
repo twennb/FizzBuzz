8db75eb543f1e40a41bdd5c51b2b798d28f4460e
twennb <tom.wennberg@hotmail.com>
2025-03-15 17:06:11 +0100

    added plan and main function structure

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,9 +1,30 @@
 """A python FizzBuzz application"""
+# plan:
+# Get user input for how many numbers to print
+# Print 1 - user defined max
+# Any number divisible by 3 = Fizz
+# Any number divisible by 5 = Buzz
+# Any number divisible by both = FizzBuzz
+
+
+def get_input():
+    """Function gets user defined max number"""
+
+
+def fizz():
+    """Function prints Fizz instead of a number divisible by 3"""
+
+
+def buzz():
+    """Function prints Buzz instead of a number divisible by 5"""
+
+
+def fizzbuzz():
+    """Function prints FizzBuzz instead of a number divisible by 3 and 5"""
 
 
 def main():
     """the main function"""
-    # code
 
 
 if __name__ == "__main__":
