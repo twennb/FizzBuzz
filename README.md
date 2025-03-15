09f70564e8fd2810efa7d9658a458f5a4f336282
twennb <tom.wennberg@hotmail.com>
2025-03-15 17:27:14 +0100

    Finished the program. Filled in functions for the various operations. Wrote readme.

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # FizzBuzz
 Prints "Fizz" instead of numbers that are a multiple of 3, "Buzz" for multiple of 5, and "FizzBuzz" for multiples of both.
+
+## How to Run the App
+
+1. Make sure you have Python installed (Python 3 recommended).  
+2. Clone this repository or download the `fizzbuzz.py` file.  
+3. Open a terminal or command prompt.  
+4. Navigate to the project directory and run:
+
+  **Windows:**
+   ```sh
+   python fizzbuzz.py
+   ```
+  **MacOS:**
+   ```sh
+   python3 fizzbuzz.py
+   ```
+  **Linux:**
+   ```sh
+   python3 fizzbuzz.py
+   ```
+5. Follow the prompts!
+
+## Features
+
+*Asks user how high the program should count
+*Prints each number, Fizz, Buzz, or FizzBuzz as appropriate
+
+## Possible Improvements
+
+* Improve number evaluation efficiency 
--- fizzbuzz.py
+++ fizzbuzz.py
@@ -9,22 +9,51 @@
 
 def get_input():
     """Function gets user defined max number"""
+    while True:
+        try:
+            range_max = int(input("\nHow high should I count: "))
+            return range_max
+        except ValueError:
+            print("Invalid input! Please enter a number.")
 
 
-def fizz():
+def count_and_evaluate_numbers(range_max, altered_range):
+    """Function counts through all numbers up to user defined max and decides whether to replace"""
+    for num in range(1, range_max+1):
+        if num % 5 == 0 and num % 3 == 0:
+            fizzbuzz(altered_range)
+        elif num % 5 == 0:
+            buzz(altered_range)
+        elif num % 3 == 0:
+            fizz(altered_range)
+        else:
+            altered_range.append(num)
+
+
+def fizz(altered_range):
     """Function prints Fizz instead of a number divisible by 3"""
+    altered_range.append("Fizz")
 
 
-def buzz():
+def buzz(altered_range):
     """Function prints Buzz instead of a number divisible by 5"""
+    altered_range.append("Buzz")
 
 
-def fizzbuzz():
+def fizzbuzz(altered_range):
     """Function prints FizzBuzz instead of a number divisible by 3 and 5"""
+    altered_range.append("FizzBuzz")
 
 
 def main():
     """the main function"""
+    altered_range = []
+
+    range_max = get_input()
+    count_and_evaluate_numbers(range_max, altered_range)
+
+    for num in altered_range:
+        print(num)
 
 
 if __name__ == "__main__":
